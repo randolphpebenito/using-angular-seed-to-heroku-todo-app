0a6036e8c306e2d7d140e44646a1bc29f14fcccf
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-25 12:21:00 +0800

    Renamed app.js to app.module.js

---
+++ app/app.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular
+    .module('todoApp', [
+        'ngRoute',
+        'ngResource',
+		'todoList',
+    ]
+);
+
