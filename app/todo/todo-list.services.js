e59382413269dcd5abfad9955847ed219dd588f9
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-27 18:00:30 +0800

    Updated resource URL.

--- app/todo/todo-list.services.js
+++ app/todo/todo-list.services.js
@@ -2,23 +2,18 @@
 
 angular.module('todoList').factory(
     'Todo', function($resource){
-            var url = 'http://intellij-todo-randolph.herokuapp.com/api/todo/'
-            return $resource(url, {}, {
-                query: {
-                    method: "GET",
-                    params: {},
-                    isArray: true,
-                    cache: true,
-                    // transformResponse
-                    // interceptor
-                },
-                get: {
-                    method: "GET",
-                    // params: {"id": @id},
-                    isArray: true,
-                    cache: true,
+            var url = 'http://intellij-todo-randolph.herokuapp.com/api/todo/:id'
+
+           // var url = 'http://127.0.0.1:8000/api/todo/:id'
+            return $resource(
+                url, 
+                null, 
+                {
+                    'update': { 
+                        method:'PUT' 
+                    }
                 }
-            })
+            )
     }
 );
 
