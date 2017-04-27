7e5b7098f6425622eb44e42a3ddee75990afb6a6
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-27 17:30:00 +0800

    Added CRUD functionality using .

--- app/todo/todo-list.component.js
+++ app/todo/todo-list.component.js
@@ -4,13 +4,54 @@ angular
     .module('todoList')
     .component('todoList', {
         templateUrl: 'todo/todo-list.html',
-        controller: function ($scope, Todo) {
-            $scope.hello = 'this is a component again!';
+        controller: function ($scope, Todo, $location) {
+
+            //List All Todo
 			$scope.todos = Todo.query();
+
+            //Create Todo
+            $scope.addTodo = function() {
+                console.log('inside addTodo');
+                if(!$scope.newTodo || $scope.newTodo.length < 1) return;
+                var todo = new Todo({ title: $scope.newTodo, completed: false });
+                todo.$save(function(){
+                  $scope.todos.unshift(todo);
+                  $scope.newTodo = ''; // clear textbox
+                });
+            }
+
+            //Retrieve Todo
+            $scope.viewEditTodo = function(todo_id, index) {
+                $scope.todo = Todo.get({ id: todo_id });
+                $scope.todoIndex = index;
+            }  
+
+            //Update Todo Status
+            $scope.updateTodoStatus = function(todo) {
+                Todo.update({id: todo.id}, todo, function(){
+                    console.log("Success");
+                });
+            }  
+
+            //Update Todo Details
+            $scope.updateTodo = function(todo_id) {
+                $scope.todo = Todo.get({ id: todo_id });
+            }  
+
+            //Delete Todo
+            $scope.removeTodo = function(todo_id, data) {
+                Todo.remove({id: todo_id}, function(){
+                    if (data.isModal) {
+                        angular.element('#todo-info').modal('hide');
+                    }
+                    $scope.todos.splice(data.index, 1);
+                });
+
+                
+            }
         }
 
     }
-
 
 );
 
